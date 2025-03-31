de0a92036afebfcc585f960af82f8350165ac0e3
PPS27 <prajnansharma450@gmail.com>
2025-03-31 19:20:38 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-function showMessage() {
-    document.getElementById("message").innerText = "Hello! You clicked the button!";
+let display = document.getElementById("display");
+let scientificVisible = false;
+
+function appendCharacter(char) {
+    display.value += char;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
+
+function toggleScientific() {
+    scientificVisible = !scientificVisible;
+    document.querySelector(".scientific-buttons").style.display = scientificVisible ? "grid" : "none";
 }
